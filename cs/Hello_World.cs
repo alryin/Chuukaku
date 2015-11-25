81c85f9600b22cf60b0fbef69953ee6c60b93594
Alessio Riccardo <alessiorvilla@gmail.com>
2015-11-25 11:43:06 +0100

    Update and rename Hello World to Hello_World.cs

--- cs/Hello_World.cs
+++ cs/Hello_World.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace esempio_starter
 {
-    class Es_1   {
+    class Hello_World {
         static void Main(string[] args) {
             string str = "\\o-o/ Praise the sun!";
             Console.WriteLine(str);
