bf5495413bda0ff26b8dd3d0c4bd36ab13f1c812
DavideP <dxp420@gmail.com>
2015-12-04 15:52:51 +0100

    Create Es1.cs

---
+++ cs/04-12-2015/Es1.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+
+    enum livello { veryLow = 1, low, medium, high, highest }
+
+    class Program
+    {
+
+        string nome;
+        string cognome;
+        int matricola;
+        livello level;
+
+  
+        public Program(string nome, string cognome, int matricola, livello level)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.matricola = matricola;
+            this.level = level;
+        }
+
+        public void setNome(string nome){ this.nome = nome; }
+        public void setCognome(string cognome) { this.cognome = cognome; }
+        public void setMatricola(int matricola) { this.matricola = matricola; }
+
+        public string getNome() { return nome; }
+        public string getCognome() { return cognome; }
+        public int getMatricola() { return matricola; }
+
+        public override string ToString()
+        {
+            string generali = "nome: " + nome + "\ncognome: " + cognome + "\nMatricola: " + matricola +
+                "\nLivello Di Sicurezza: " + level ;
+            return generali;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            Program p1 = new Program("E", "Mess", 0004, livello.veryLow);
+            Console.WriteLine(p1.ToString());
+
+            
+
+
+        }
+    }
+}
