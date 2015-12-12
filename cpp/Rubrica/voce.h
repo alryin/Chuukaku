67ca0cbfd35099696cd230c7f7e2a8d707ae5914
Alessio Riccardo <alessiorvilla@gmail.com>
2015-12-12 11:49:49 +0100

    Create voce.h

---
+++ cpp/Rubrica/voce.h
@@ -0,0 +1,27 @@
+/*
+version 1.0
+
+Written by Ryick 
+(add your name and modify the verion number if you modify the file)
+*/
+#ifndef VOCE_H
+#define VOCE_H
+
+#include <string>
+#include <ostream>
+
+using namespace std;
+
+struct voce {
+	string nome;
+	string cognome;
+	string ntel;
+
+	voce();
+	voce(string nome, string cognome, string ntel);
+
+	ostream& operator<< (ostream& os, const voce& v);
+};
+
+#endif
+//eof voce.h
