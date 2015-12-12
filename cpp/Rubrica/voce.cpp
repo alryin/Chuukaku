160efca4fc4130d0ff49e250970a262a0f282fcc
Alessio Riccardo <alessiorvilla@gmail.com>
2015-12-12 18:31:42 +0100

    Update voce.cpp

--- cpp/Rubrica/voce.cpp
+++ cpp/Rubrica/voce.cpp
@@ -1,22 +1,24 @@
 /*
-version 2.0
+version 2.1
 
 Written by Ryick 
 
 (add your name and modify the version number if you update the file)
 */
+
 #include "voce.h"
 
 using namespace std;
+//implementation of voce.h methods
 
 //more efficient than initialization
-voce() : 
+Voce::Voce() : 
 	nome(), cognome(), ntel() { }
 
-voce(string nome, string cognome, string ntel): 
+Voce::Voce(string nome, string cognome, string ntel): 
 	nome(nome), cognome(cognome), ntel(ntel) { }
 
-ostream& operator<< (ostream& os, const voce& v) {
+ostream& operator<< (ostream& os, const Voce& v) {
 	os << v.nome << " " << v.cognome << " " << v.ntel;
 	return os;
 }
